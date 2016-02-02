bf0a437cf2471d6c64ad6a40ccb3c83f3e7ef60a
vichaoctavia <octaviavicha@gmail.com>
2016-02-02 23:14:13 +0700

    Update Project.java

--- Exercise_2/Project.java
+++ Exercise_2/Project.java
@@ -1,1 +1,46 @@
-
+//Vicha Octavia Tama_1301144209_IF-38-09
+  
+ public class Project 
+ {
+ 	private String projectName;
+	private int nTeam=0;
+ 	private boolean releaseStatus;
+ 	private Member[] teamMember;
+ 	
+ 	public Project (String projectName) 
+	{
+ 		this.projectName = projectName;
+ 		teamMember = new Member[5];
+ 		releaseStatus = false;
+ 	}
+ 	
+ 	public void addMember(Member mm)
+	{
+ 		teamMember[nTeam]=mm;
+ 		nTeam++;
+ 	}
+ 	
+ 	public boolean isReleased () 
+	{
+ 		return releaseStatus;
+	}
+ 	
+ 	public void releaseApp() 
+	{
+ 		releaseStatus = true;
+ 	}
+ 	
+ 	public String toString() 
+	{
+         String status;
+         if(releaseStatus = true) 
+		 {
+             status="Released";
+         }
+         else 
+		 {
+             status="in Progress";
+         }
+ 		return ("Project "+projectName+" status is "+status+" with team member of "+nTeam);
+ 	}
+ }
