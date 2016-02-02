209b1f9e2773bdc6d0203ac18fb8b8777a71b9a9
vichaoctavia <octaviavicha@gmail.com>
2016-02-02 21:05:20 +0700

    Update SmartPhone.java

--- Exercise_1/SmartPhone.java
+++ Exercise_1/SmartPhone.java
@@ -1,1 +1,37 @@
+//Vicha Octavia Tama_1301144209_IF-38-09
 
+public class Smartphone {
+	
+	private Application[] appList=new Application[100];
+	private int memory;
+	private int totalApp;
+	
+	public void setMemory(int memory)
+	{
+		this.memory=memory;
+	}
+	public int getRemainingSize()
+	{
+		int m=memory;
+		int i;
+		int memoryUse;
+		i=0;
+		memoryUse=0;
+		while(appList[i]!=null) {
+			memoryUse=memoryUse+appList[i].getAppSize();
+			i++;
+		}
+		return m=m-memoryUse;
+	}
+	public void addApplication(AppStore appStore, int appId)
+	{
+		if(getRemainingSize()>=appStore.getApp(appId).getAppSize())
+		{
+			appList[totalApp]=appStore.getApp(appId);
+		}
+	}
+	public String toString()
+	{
+		return("Memory size "+memory+", "+totalApp+" application installed, remaining memory size : "+getRemainingSize());
+	}
+}
