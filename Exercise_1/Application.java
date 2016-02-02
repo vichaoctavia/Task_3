ad68d82e8ffd174a1eeb4f77d5b83273a0058cf6
vichaoctavia <octaviavicha@gmail.com>
2016-02-02 21:04:49 +0700

    Update Application.java

--- Exercise_1/Application.java
+++ Exercise_1/Application.java
@@ -1,1 +1,28 @@
+//Vicha Octavia Tama_1301144209_IF-38-09
 
+public class Application
+{
+	private String appName;
+	private int appSize;
+	
+	public void Application(String appName, int appSize)
+	{
+		this.appName = appName;
+		this.appSize = appSize;
+	}
+ 	
+	public String getAppName()
+	{
+		return appName;
+	}
+	
+	public int getAppSize()
+	{
+		return appSize;
+	}
+ 	
+	public String toString()
+	{
+ 		return ("Application name : "+appName+", with size : "+appSize);
+ 	}
+ }
