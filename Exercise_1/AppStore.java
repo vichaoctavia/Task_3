5410b2e4d15f734e0400acafccd9a9db6b565dd4
vichaoctavia <octaviavicha@gmail.com>
2016-02-02 21:03:47 +0700

    Update AppStore.java

--- Exercise_1/AppStore.java
+++ Exercise_1/AppStore.java
@@ -1,1 +1,29 @@
-
+//Vicha Octavia Tama_1301144209_IF-38-09
+ 
+ public class AppStore
+ {
+	private Application[] appList = new Application[100];
+	private int totalApp = 0;
+ 	
+	public void createNewApp(String appName, int appSize)
+	{
+		Application app = new Application();
+		appList[totalApp] = app; totalApp++;
+	}
+	public Application getApp(int id)
+	{
+		return appList[id];
+	}
+ 	public int getTotalApp() 
+	{
+		return totalApp;
+ 	}
+	public Application[] getAppList()
+	{
+ 		return appList;
+ 	}
+ 	public String toString()
+	{
+ 		return ("There are "+totalApp+" applications ready to install");
+ 	}
+ }	
