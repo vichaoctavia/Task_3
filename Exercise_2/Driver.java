72bd61d19940317fe3afead7db8e08acfb1f423f
vichaoctavia <octaviavicha@gmail.com>
2016-02-02 23:55:31 +0700

    Update Driver.java

--- Exercise_2/Driver.java
+++ Exercise_2/Driver.java
@@ -1,1 +1,35 @@
-
+//Vicha Octavia Tama_1301144209_IF-38-09
+ 
+ public class Driver 
+ {
+ 	public static void main(String[] args ) 
+ 	{
+		StartUp stp= new StartUp();
+ 		Member m = new Member("Alex");
+ 		stp.addMember(m);
+ 		Member m = new Member("Beni");
+ 		stp.addMember(m);
+ 		Member m = new Member("Cakra");
+ 		stp.addMember(m);
+ 		Member m = new Member("Deni");
+ 		stp.addMember(m);
+ 		Member m = new Member("Eric");
+ 		stp.addMember(m);
+ 			
+ 		stp.createNewProject("Project1");
+ 		stp.createNewProject("Project2");
+ 			
+ 		Project p = stp.getProject(0);
+ 		m = stp.getMember(0);
+ 		stp.setProjectMember(p,m);
+ 			
+ 		stp.releaseProject(stp.getProject(1));
+		
+ 		System.out.println(stp.getMember(0).toString());
+ 		System.out.println(stp.getMember(1).toString());
+ 		System.out.println(stp.getMember(2).toString());
+ 		System.out.println(stp.getMember(3).toString());
+ 		System.out.println(stp.getMember(4).toString());
+ 		System.out.println("Number of project released is : "+stp.getNumReleasedProject());
+ 	}
+ }
